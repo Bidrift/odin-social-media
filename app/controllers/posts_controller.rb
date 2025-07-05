c052ac0fbeb0436547695c4d17eaf47c5f3784e9
Bidrift <yassine.fighter@gmail.com>
2025-07-05 13:12:40 +0100

    Add posts controller with routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
