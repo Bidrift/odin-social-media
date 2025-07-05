3206f209fc7f98b3d9bc38e8e2bf3670715388c8
Bidrift <yassine.fighter@gmail.com>
2025-07-05 13:05:20 +0100

    Add Post model and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: "creator_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true, presence: true, 
+        format: { with: /\A[a-zA-Z][a-zA-Z0-9_]+\z/,
+                  message: "is invalid (only letters, digits and underscore)"},
+        length: { in: 4..16 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_05_112105) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_05_115909) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "creator_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_posts_on_creator_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +32,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_05_112105) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users", column: "creator_id"
 end
