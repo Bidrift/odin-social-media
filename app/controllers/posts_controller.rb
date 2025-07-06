7f44d8cb001ed7ee53ed8977b57dea31d423d552
Bidrift <yassine.fighter@gmail.com>
2025-07-06 10:33:01 +0100

    Add Posts controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,70 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :authorize_user, only: [:create, :update]
+  
   def index
+    @posts = Post.all.reverse
+    if user_signed_in?
+      @post = current_user.posts.build()
+    end
   end
 
   def create
-  end
-
-  def new
+    @post = Post.new(post_params)
+    if @post.save
+      flash['success'] = "Post created successfully"
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    unless authorize_user(@post.creator_id)
+      index
+    end
+    if @post.destroy
+      flash['success'] = 'Post deleted successfully'
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+    unless authorize_user(@post.creator_id)
+      index
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash['success'] = "Post updated successfull"
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def authorize_user(param=post_params[:creator_id])
+    unless current_user.id == param.to_i
+      flash['alert'] = "You are not allowed to perform this action"
+      head(:unauthorized)
+      return false
+    end
+    true
+  end
+
+  def post_params
+    params.expect(post: [:creator_id, :body])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
+  root 'posts#index'
+  resources :posts, except: [:new]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
