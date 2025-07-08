44b17353d3c9e761848636130c2919209c2214cc
Bidrift <yassine.fighter@gmail.com>
2025-07-08 14:26:13 +0100

    Add bio validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,9 @@
 class Profile < ApplicationRecord
   self.primary_key = :user_id
+  validates :avatar_url, presence: true
+  validates :bio, presence: true, length: { in: 4..100 }
+  
+  
   
   belongs_to :user
 end
